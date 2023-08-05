08259e5ecdfae21b857c819a7cf30998c13ea8c1
Grace <graceofubu04@gmail.com>
2023-08-05 17:27:21 +0400

    installed firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDFs9OnDKKjKiL8mAdo8Zbg_SUlSn1KePA",
+  authDomain: "netfix-clone-96407.firebaseapp.com",
+  projectId: "netfix-clone-96407",
+  storageBucket: "netfix-clone-96407.appspot.com",
+  messagingSenderId: "489334416864",
+  appId: "1:489334416864:web:a7cf2d0247aab5fe27f785",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
