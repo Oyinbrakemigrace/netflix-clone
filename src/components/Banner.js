b52dc47bc32f5db1e56f60fe9cb0ff61ed0bee5e
Grace <graceofubu04@gmail.com>
2023-08-06 21:02:49 +0400

    netflix banner functionality and some notes

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 const Banner = () => {
+  const descriptionEllipsis = (string, numberOfString) => {
+    return string?.length > numberOfString
+      ? string.substr(0, numberOfString - 1) + "..."
+      : string; //it takes a substring from 0 to numberOfString - 1 and appends ... in place of the rest of the string.
+  };
 
-    const descriptionEllipsis = (string, numberOfString)=>{
-        return string?.length > numberOfString ? string.substr(0, numberOfString-1)+ '...' : string //it takes a substring from 0 to n-1 and appends ... to it
-    }
-  
   return (
     <header className="bg-banner bg-cover bg-center h-[448px] relative text-white object-contain">
       <div className="ml-[30px] pt-[140px]">
@@ -19,10 +20,13 @@ const Banner = () => {
           </button>
         </div>
         <h1 className="w-[720px] pt-[16px] text-[15px] max-w-[360px] h-[80px]">
-          {descriptionEllipsis(`This is a movie description Lorem ipsum dolor sit amet consectetur
+          {descriptionEllipsis(
+            `This is a movie description Lorem ipsum dolor sit amet consectetur
           adipisicing elit. Consequuntur veniam unde deleniti soluta quo, a non
           incidunt obcaecati qui deleniti earum, voluptas non possimus aliquam
-          velit totam!`, 100)}
+          velit totam!`,
+            100
+          )}
         </h1>
       </div>
       <div />
@@ -32,7 +36,8 @@ const Banner = () => {
 
 export default Banner;
 
+//The substring of a string is a portion of the original string that starts at
+//a specified index and includes a specified number of characters.
 
-
-//The substring of a string is a portion of the original string that starts at 
-//a specified index and includes a specified number of characters. 
+//string?.length checks if the string parameter is not null or
+//undefined and then accesses its length property.
